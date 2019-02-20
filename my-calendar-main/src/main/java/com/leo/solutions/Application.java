f7c735265ccd5fe7b1f6a3d2e32b5740c37949df
Love Bisaria <bisarialove@gmail.com>
2019-02-19 17:36:37 -0800

    first commit

---
+++ my-calendar-main/src/main/java/com/leo/solutions/Application.java
@@ -0,0 +1,16 @@
+package com.leo.solutions;
+/*
+ * @author love.bisaria on 07/01/19
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ my-calendar-worker/src/main/java/com/leo/solutions/WorkerApplication.java
@@ -0,0 +1,14 @@
+package com.leo.solutions;
+/*
+ * @author love.bisaria on 07/01/19
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerApplication.class, args);
+    }
+}
