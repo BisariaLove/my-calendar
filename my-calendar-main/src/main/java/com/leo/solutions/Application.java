799edbea8f28e84ed6764cabae056c6191557f67
Love Bisaria <bisarialove@gmail.com>
2019-02-20 22:46:02 -0800

    fixing app startup

--- my-calendar-main/src/main/java/com/leo/solutions/Application.java
+++ my-calendar-main/src/main/java/com/leo/solutions/Application.java
@@ -4,10 +4,25 @@ package com.leo.solutions;
  */
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@Configuration
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class})
 @SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = {"com.leo.solutions"},
+        excludeFilters ={ @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value=Application.class)}
+)
 public class Application {
 
     public static void main(String[] args) {
